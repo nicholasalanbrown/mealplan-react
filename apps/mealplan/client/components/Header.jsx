85d330b511069c3b1910186a36f2fbd70a0f6304
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-03-05 21:11:43 -0500

    Adding link logic for header

--- apps/mealplan/client/components/Header.jsx
+++ apps/mealplan/client/components/Header.jsx
@@ -11,9 +11,40 @@ export default class Header extends Component {
   }
 
   render() {
+    let linkList;
     let loginLink = <a href="/sign-in" className="pure-menu-link">Sign In</a>;
     if (Meteor.userId()) {
       loginLink = <a onClick={this.handleLogout.bind(this)} href="#" className="pure-menu-link">Sign Out</a>;
+    }
+    if (Roles.userIsInRole(Meteor.userId(), 'admins')) {
+      linkList =
+      <div className="pure-menu pure-menu-horizontal">
+          <ul className="pure-menu-list">
+              <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                  <a href="#" className="pure-menu-link">Recipes</a>
+                  <ul className="pure-menu-children">
+                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Add New</a></li>
+                      <li className="pure-menu-item"><a href="/recipes" className="pure-menu-link">View All</a></li>
+                  </ul>
+              </li>
+              <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+                  <a href="#" className="pure-menu-link">Ingredients</a>
+                  <ul className="pure-menu-children">
+                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Add New</a></li>
+                      <li className="pure-menu-item"><a href="/ingredients" className="pure-menu-link">View All</a></li>
+                  </ul>
+              </li>
+              <li className="pure-menu-item pure-menu-selected">{loginLink}</li>
+          </ul>
+      </div>
+    }
+    else {
+      linkList =
+      <div className="pure-menu pure-menu-horizontal">
+          <ul className="pure-menu-list">
+              <li className="pure-menu-item pure-menu-selected">{loginLink}</li>
+          </ul>
+      </div>
     }
     return (
       <Headroom>
@@ -21,25 +52,7 @@ export default class Header extends Component {
         <div className="brand">
           <a href="/">Meal Plan</a>
         </div>
-        <div className="pure-menu pure-menu-horizontal">
-            <ul className="pure-menu-list">
-                <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
-                    <a href="#" className="pure-menu-link">Recipes</a>
-                    <ul className="pure-menu-children">
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Add New</a></li>
-                        <li className="pure-menu-item"><a href="/recipes" className="pure-menu-link">View All</a></li>
-                    </ul>
-                </li>
-                <li className="pure-menu-item pure-menu-has-children pure-menu-allow-hover">
-                    <a href="#" className="pure-menu-link">Ingredients</a>
-                    <ul className="pure-menu-children">
-                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Add New</a></li>
-                        <li className="pure-menu-item"><a href="/ingredients" className="pure-menu-link">View All</a></li>
-                    </ul>
-                </li>
-                <li className="pure-menu-item pure-menu-selected">{loginLink}</li>
-            </ul>
-        </div>
+        {linkList}
       </nav>
       </Headroom>
     );
