6be8b1e7c41277d131105e348fa3c960a386f213
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-03-08 17:39:27 -0500

    Editing ingredients

--- apps/mealplan/client/components/ingredients/IngredientRow.jsx
+++ apps/mealplan/client/components/ingredients/IngredientRow.jsx
@@ -1,30 +1,26 @@
 import { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 
 export default class IngredientRow extends Component {
 
   state = {
-      newName: '',
       editing: false
   }
 
   handleBlur = (e) => {
-    console.log(e.target.value);
-    /*
-      Meteor.call('updateIngredient', this.props._id, e.target.value)
+      Meteor.call('updateIngredient', this.props.ingredientId, e.target.value)
       this.setState({editing: !this.state.editing})
-    */
   }
 
   handleClick = (e) => {
       this.setState({editing: !this.state.editing})
   }
 
-
   render() {
     if (this.state.editing) {
       return (
         <tr>
-          <td><input className="pure-input" onBlur={this.handleBlur.bind(this)} value={this.state.name} /></td>
+          <td><input ref="nameInput" className="pure-input" onBlur={this.handleBlur.bind(this)} defaultValue={this.props.name} autoFocus={true} /></td>
         </tr>
       );
     }
