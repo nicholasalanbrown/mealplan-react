8c612ce1d0d6f9742b2c1a418fdf793e5c5cc82b
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-03-08 17:27:32 -0500

    More ingredient editing

--- apps/mealplan/client/components/ingredients/IngredientRow.jsx
+++ apps/mealplan/client/components/ingredients/IngredientRow.jsx
@@ -8,19 +8,23 @@ export default class IngredientRow extends Component {
   }
 
   handleBlur = (e) => {
+    console.log(e.target.value);
+    /*
       Meteor.call('updateIngredient', this.props._id, e.target.value)
       this.setState({editing: !this.state.editing})
+    */
   }
 
   handleClick = (e) => {
       this.setState({editing: !this.state.editing})
   }
 
+
   render() {
     if (this.state.editing) {
       return (
         <tr>
-          <td><input onBlur={this.handleBlur.bind(this)} value={this.props.name} /></td>
+          <td><input className="pure-input" onBlur={this.handleBlur.bind(this)} value={this.state.name} /></td>
         </tr>
       );
     }
