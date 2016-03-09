0ddd70d2662a5d9a27bd4b678ba924c195af59f1
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-03-09 14:53:28 -0500

    Deleting recipes

--- apps/mealplan/client/components/recipes/RecipeRow.jsx
+++ apps/mealplan/client/components/recipes/RecipeRow.jsx
@@ -2,10 +2,15 @@ import { Component, PropTypes } from 'react';
 
 export default class RecipeList extends Component {
 
+
+  deleteRecipe = (e) => {
+    Meteor.call('deleteRecipe', this.props.recipeId);
+  }
+
   render() {
     return (
       <tr>
-        <td><a href={FlowRouter.path('viewRecipe', {recipeId: this.props.recipeId})}>{this.props.title}</a></td>
+        <td><a href={FlowRouter.path('viewRecipe', {recipeId: this.props.recipeId})}>{this.props.title}</a></td><td><a href="" onClick={this.deleteRecipe.bind(this)}><i className="fa fa-trash"/></a></td>
       </tr>
     );
   }
