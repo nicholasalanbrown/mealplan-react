8941dbd98e0a1f1df598026a0e299f004520aab9
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-03-13 23:18:04 -0400

    Fixing body scrolling when menu is open

--- apps/mealplan/client/components/Menu.jsx
+++ apps/mealplan/client/components/Menu.jsx
@@ -1,10 +1,16 @@
 import { Component } from 'react';
+import BodyClassName  from 'react-body-classname';
 
 export default class Menu extends Component {
 
   render() {
+    let bodyNoScroll;
+    if (this.props.visible) {
+      bodyNoScroll = <BodyClassName className='body-no-scroll' />
+    }
     return (
       <div className="menu">
+        {bodyNoScroll}
         <div className={(this.props.visible ? "visible " : "") + this.props.alignment}>{this.props.children}</div>
       </div>
     );
