6416fe128c7f12f73fa1c8eb6209edb6cd08f7d6
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-03-19 14:58:07 -0400

    Switching to CSS modules

--- apps/mealplan/client/components/Home.jsx
+++ apps/mealplan/client/components/Home.jsx
@@ -4,6 +4,8 @@ import ReactMixin from 'react-mixin';
 import HomeLoggedIn from './HomeLoggedIn';
 import Content from './Content';
 
+import '../styles/modules/welcome.scss';
+
 @ReactMixin.decorate(ReactMeteorData)
 export default class Home extends Component {
 
@@ -33,7 +35,7 @@ export default class Home extends Component {
     }
     else {
       return (
-        <div className="welcome-background">
+        <div className="welcomeBackground">
           <div className="pure-g full-container">
             <div className="pure-u-1-24 pure-u-md-6-24 pure-u-lg-8-24"></div>
             <div className="pure-u-22-24 pure-u-md-12-24 pure-u-lg-8-24 welcome-cta">
