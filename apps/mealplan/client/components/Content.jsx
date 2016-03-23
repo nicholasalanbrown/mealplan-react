5c83e51c1f97f4534992771e3b7639361e894f33
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-03-23 17:15:10 -0400

    Better home layout

--- apps/mealplan/client/components/Content.jsx
+++ apps/mealplan/client/components/Content.jsx
@@ -5,11 +5,11 @@ export default class Content extends Component {
   render() {
     return (
       <div className="pure-g content-wrapper">
-        <div className="pure-u-2-24 pure-u-md-4-24 pure-u-lg-6-24"></div>
-        <div className="pure-u-20-24 pure-u-md-16-24 pure-u-lg-12-24">
+        <div className="pure-u-2-24 pure-u-md-2-24 pure-u-lg-4-24"></div>
+        <div className="pure-u-20-24 pure-u-md-20-24 pure-u-lg-16-24">
           {this.props.children}
         </div>
-        <div className="pure-u-2-24 pure-u-md-4-24 pure-u-lg-6-24"></div>
+        <div className="pure-u-2-24 pure-u-md-2-24 pure-u-lg-4-24"></div>
       </div>
     );
   }
