d0a20321d1b0e5aaad1199622605383cb93679a9
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-03-23 17:36:21 -0400

    Less padding for content on mobile

--- apps/mealplan/client/components/Content.jsx
+++ apps/mealplan/client/components/Content.jsx
@@ -5,11 +5,11 @@ export default class Content extends Component {
   render() {
     return (
       <div className="pure-g content-wrapper">
-        <div className="pure-u-2-24 pure-u-md-2-24 pure-u-lg-4-24"></div>
-        <div className="pure-u-20-24 pure-u-md-20-24 pure-u-lg-16-24">
+        <div className="pure-u-1-24 pure-u-md-2-24 pure-u-lg-4-24"></div>
+        <div className="pure-u-22-24 pure-u-md-20-24 pure-u-lg-16-24">
           {this.props.children}
         </div>
-        <div className="pure-u-2-24 pure-u-md-2-24 pure-u-lg-4-24"></div>
+        <div className="pure-u-1-24 pure-u-md-2-24 pure-u-lg-4-24"></div>
       </div>
     );
   }
